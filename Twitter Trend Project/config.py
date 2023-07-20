0d17c6b557c68a8f1c16820aebf1003a3cb780f1
Malcolm Lewis <Lewismalcolm32@gmail.com>
2023-07-20 01:05:03 -0400

    Testing .gitignore

--- Twitter Trend Project/config.py
+++ Twitter Trend Project/config.py
@@ -4,3 +4,4 @@ ACCESS_TOKEN = '37060595-FlShwh35livHmS7hozzhUSCViWEhJ4C7feLpdtBjW' #Username (T
 ACCESS_SECRET = 'AwcydUYWbIhDBNoChahtyZ0u7d97NGA5eCsTvWem1WL7B' #Password (Twitter Account)
 CONSUMER_KEY = 'jnTEuu5nSRGwXyEHbLBjvgzHw' #API Key (Twitter App User Name)
 CONSUMER_SECRET = 'DKahlLSTUpS3jvgq8FGQ8E5rpJ22bY7upKOQM364TIU2O8IUlE' #API Secret Key (Twitter App Password)
+#jthfgjv

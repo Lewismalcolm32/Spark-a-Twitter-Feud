774009448130a23a013c2e8ebd5daeaa5e710d81
Malcolm Lewis <Lewismalcolm32@gmail.com>
2023-07-20 01:00:05 -0400

    Adding some security

--- Twitter Trend Project/TwitterTrends.py
+++ Twitter Trend Project/TwitterTrends.py
@@ -6,11 +6,12 @@ import sys
 import requests #HTTP Requests
 import requests_oauthlib
 import json
+import config
 
-ACCESS_TOKEN = '37060595-FlShwh35livHmS7hozzhUSCViWEhJ4C7feLpdtBjW' #Username (Twitter Account)
-ACCESS_SECRET = 'AwcydUYWbIhDBNoChahtyZ0u7d97NGA5eCsTvWem1WL7B' #Password (Twitter Account)
-CONSUMER_KEY = 'jnTEuu5nSRGwXyEHbLBjvgzHw' #API Key (Twitter App User Name)
-CONSUMER_SECRET = 'DKahlLSTUpS3jvgq8FGQ8E5rpJ22bY7upKOQM364TIU2O8IUlE' #API Secret Key (Twitter App Password)
+ACCESS_TOKEN = config.ACCESS_TOKEN #Username (Twitter Account)
+ACCESS_SECRET = config.ACCESS_SECRET #Password (Twitter Account)
+CONSUMER_KEY = config.CONSUMER_KEY #API Key (Twitter App User Name)
+CONSUMER_SECRET = config.CONSUMER_SECRET #API Secret Key (Twitter App Password)
 my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
 
 #Create get_tweets function to call Twitter API URL, and return the response for a stream of Tweets
--- Twitter Trend Project/config.py
+++
@@ -1,6 +0,0 @@
-# configuration file for Twitter Trend Project
-
-ACCESS_TOKEN = '37060595-FlShwh35livHmS7hozzhUSCViWEhJ4C7feLpdtBjW' #Username (Twitter Account)
-ACCESS_SECRET = 'AwcydUYWbIhDBNoChahtyZ0u7d97NGA5eCsTvWem1WL7B' #Password (Twitter Account)
-CONSUMER_KEY = 'jnTEuu5nSRGwXyEHbLBjvgzHw' #API Key (Twitter App User Name)
-CONSUMER_SECRET = 'DKahlLSTUpS3jvgq8FGQ8E5rpJ22bY7upKOQM364TIU2O8IUlE' #API Secret Key (Twitter App Password)
